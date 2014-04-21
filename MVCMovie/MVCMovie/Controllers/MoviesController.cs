a55758794b018ece938e214009996ee7eeccdc18
Michael M <mkibabu@gmail.com>
2014-04-21 17:20:29 -0400

    5: Access Model data from a Controller

---
+++ MVCMovie/MVCMovie/Controllers/MoviesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMovie.Models;
+using MVCMovie.MovieDBContext_MVCMovie;
+
+// Through "scaffolding", when a Controller is created with the template option
+// "MVC Controller with read/write actions and views, usinf Entity Framework",
+// ASP.NET automagically creates the CRUD action methods and the views for each.
+namespace MVCMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private Models_ db = new Models_();
+
+        //
+        // GET: /Movies/
+
+        public ActionResult Index()
+        {
+            return View(db.Movies.ToList());
+        }
+
+        //
+        // GET: /Movies/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        //
+        // GET: /Movies/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Movies/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movie);
+        }
+
+        //
+        // GET: /Movies/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        //
+        // POST: /Movies/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        //
+        // GET: /Movies/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        //
+        // POST: /Movies/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVCMovie/MVCMovie/Models/Movie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+// .NET uses a data-access framework known as the Entity Framework. It allows for
+// model classes to be developed with a Code First paradigm, which allows one to
+// create model objects by writing simple classes known as POCOs (Plain Old CLR Objects).
+// The database is then derived on the fly from the classes, allowing a clean
+// and rapid development process.
+namespace MVCMovie.Models
+{
+    /// <summary>
+    /// Represents movies in the database. Each instance of a Movie object will
+    /// correspond to a row within the database table, and each property of the
+    /// class corresponds to a column within that table.
+    /// </summary>
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set;  }
+    }
+
+    /// <summary>
+    /// Represents the EntityFramework movie database context, which handles
+    /// CRUD operations on the Movie class instances within the database. Note
+    /// that "context" is used here to mean something that encapsulates state;
+    /// hence, MovieDBContext encapsulates the state of the Movie database, the
+    /// connection to the underlying data store, and a record of changes to the
+    /// objects. Therefore, MovieDBContext holds the state of the Movie database.
+    /// It handles the task of connecting to the database and mapping Movie objects
+    /// to database records. The conneection info is found in the root Web.config
+    /// file (not the /Views/Web.config file).
+    /// </summary>
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
